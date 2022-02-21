80d0236962b76025fd788b910246489128ce0554
Ramadan <jaafariramadan@gmail.com>
2022-02-21 17:41:08 +0200

    implemented BMPImage main functionallity

--- bmp_crypto.cpp
+++ bmp_crypto.cpp
@@ -1,2 +1,59 @@
 #include "bmp_crypto.h"
 
+#include <fstream>
+
+/* -=-=-=-=-=-=-=-=- BMPImage -=-=-=-=-=-=-=-=- */
+
+BMPImage::BMPImage()
+{
+    prefix = "";
+    image = "";
+}
+
+BMPImage::BMPImage(const std::string& image_path)
+{
+    updateImage(image_path);
+}
+
+// TODO
+void BMPImage::writeImage(const std::string& out_image_path)
+{
+
+}
+
+// TODO check file type
+void BMPImage::updateImage(const std::string& image_path)
+{
+    int pixels_offest; // image pixels offset, end of image info
+    std::string img = readImageFile(image_path);
+    // check image type 
+
+    // set img data
+    pixels_offest = img[10] + img[11] * 256 + img[12] * 256*256 + img[13] * 256*256*256;
+    prefix = img.substr(0, pixels_offest);
+    image = img.substr(pixels_offest, img.size() - pixels_offest);
+}
+
+// TODO do error checking
+std::string BMPImage::readImageFile(const std::string& image_path)
+{
+    char* img;
+    int file_length;
+    std::ifstream iimg(image_path);
+
+    // check for error
+
+    // get length of file:
+    iimg.seekg (0, iimg.end);
+    file_length = iimg.tellg();
+    iimg.seekg (0, iimg.beg);
+
+    // read and return file
+    iimg.read(img, file_length);
+    return static_cast<std::string>(img);
+}
+
+void BMPImage::setImg(std::string img)
+{
+    image = img;
+}
--- bmp_crypto.h
+++ bmp_crypto.h
@@ -1,29 +1,31 @@
 #ifndef __BMP_CRYPTO__
 #define __BMP_CRYPTO__
 
+#include <string>
+
 class BMPImage
 {
 public:
     BMPImage();
     /* Reads the bmp image with the path image_path
        If the path is invalid or the file is not a BMP file:
-            image_width and image_length are set to 0,
             prefix and image are set to ""
     */
-    BMPImage(std::string image_path);
+    BMPImage(const std::string& image_path);
 
     /* Use a new image at path image_path, acts like the constructor*/
-    void updateImage(std::string image_path);
+    void updateImage(const std::string& image_path);
+    void writeImage(const std::string& out_image_path);
+
+    std::string getImg();
+    void setImg(std::string img);
 private:
-    void readImageFile();
-    void readImagePrefix();
-    void readPixels();
+    std::string readImageFile(const std::string& image_path);
     
-    short image_width;  // Image width
-    short image_length; // Image length 
     std::string prefix; // Image information
     std::string image;  // Image pixels data
 };
+
 
 class BMPCrypto
 {
